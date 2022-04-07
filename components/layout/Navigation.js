54e2086356ca3e907de1ecfa3a206b0dd07acae6
Daniel Batista <dani_batista20@hotmail.com>
2022-04-07 11:49:07 +0100

    upd: Navigation - fixed nav

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -1,20 +1,22 @@
+import Link from 'next/link'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
-import Link from 'next/link'
 
 const Navigation = () => {
+  //
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
       <Container>
         <Link href="/" passHref>
           <Navbar.Brand>NextJS</Navbar.Brand>
         </Link>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
             <Link href="/" passHref>
@@ -27,7 +29,9 @@ const Navigation = () => {
               <Nav.Link>Menu</Nav.Link>
             </Link>
 
-            <Nav.Link disabled className='d-none d-lg-flex'>|</Nav.Link>
+            <Nav.Link disabled className="d-none d-lg-flex">
+              |
+            </Nav.Link>
 
             <Link href="/events" passHref>
               <Nav.Link>Events</Nav.Link>
@@ -41,23 +45,8 @@ const Navigation = () => {
             <Link href="/contact" passHref>
               <Nav.Link>Contact</Nav.Link>
             </Link>
-
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <Link href="#action/3.1" passHref>
-                <NavDropdown.Item>Action</NavDropdown.Item>
-              </Link>
-              <Link href="#action/3.2" passHref>
-                <NavDropdown.Item>Action</NavDropdown.Item>
-              </Link>
-              <Link href="#action/3.3" passHref>
-                <NavDropdown.Item>Action</NavDropdown.Item>
-              </Link>
-              <NavDropdown.Divider />
-              <Link href="#action/3.4" passHref>
-                <NavDropdown.Item>Action</NavDropdown.Item>
-              </Link>
-            </NavDropdown> */}
           </Nav>
+
           <Form className="d-flex">
             <FormControl
               type="search"
