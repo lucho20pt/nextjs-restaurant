01130c108441bfe35c07517c2dd99cd498a177d1
Daniel Batista <dani_batista20@hotmail.com>
2022-04-21 08:36:04 +0100

    upd: models/Product

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,45 +1,48 @@
 import mongoose from 'mongoose'
 
 /* PetSchema will correspond to a collection in your MongoDB database. */
-const ProductSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Please provide a Title for this Product.'],
-    maxlength: [60, 'Title cannot be more than 60 characters']
-  },
-  desc: {
-    type: String,
-    required: [true, 'Please provide a Description for this Product.'],
-    maxlength: [260, 'Description cannot be more than 260 characters']
-  },
-  image: {
-    type: String,
-    required: [true, 'Please provide an Image url for this Product.']
-  },
-  price: {
-    type: [Number],
-    required: [true, 'Please provide a Price for this Product.']
-  },
-  extraOptions: {
-    type: [
-      {
-        text: {
-          type: String,
-          required: [
-            true,
-            'Please provide a Text for this Product Extra Option.'
-          ]
-        },
-        price: {
-          type: Number,
-          required: [
-            true,
-            'Please provide a Price for this Product Extra Option.'
-          ]
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please provide a Title for this Product.'],
+      maxlength: [60, 'Title cannot be more than 60 characters']
+    },
+    desc: {
+      type: String,
+      required: [true, 'Please provide a Description for this Product.'],
+      maxlength: [260, 'Description cannot be more than 260 characters']
+    },
+    image: {
+      type: String,
+      required: [true, 'Please provide an Image url for this Product.']
+    },
+    price: {
+      type: [Number],
+      required: [true, 'Please provide a Price for this Product.']
+    },
+    extraOptions: {
+      type: [
+        {
+          text: {
+            type: String,
+            required: [
+              true,
+              'Please provide a Text for this Product Extra Option.'
+            ]
+          },
+          price: {
+            type: Number,
+            required: [
+              true,
+              'Please provide a Price for this Product Extra Option.'
+            ]
+          }
         }
-      }
-    ]
-  }
-})
+      ]
+    }
+  },
+  { timestamps: true }
+)
 
-export default mongoose.models.Product || mongoose.model('Product', PetSchema)
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema)
